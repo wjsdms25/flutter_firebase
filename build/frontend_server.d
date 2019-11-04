b8fc05a5f0299d25b433b124916301f1fb9bd02a
yeeun <21500631@handong.edu>
2019-11-04 22:42:04 +0900

    commit

---
+++ build/frontend_server.d
@@ -0,0 +1,1 @@
+frontend_server.d: /Users/jeon-ye/development/tools/flutter/bin/cache/artifacts/engine/darwin-x64/frontend_server.dart.snapshot
---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/jeon-ye/development/tools/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/jeon-ye/AndroidStudioProjects/final_/mdc_100_series_2"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/jeon-ye/development/tools/flutter/bin/cache/artifacts/engine/ios"
